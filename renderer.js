e11419141a14ecd53b2868d2e9b1822cc8b695f8
chlzd <chlouzada@gmail.com>
2021-06-25 08:58:41 -0300

    feat: apresentação dos filmes com seus ratings

--- renderer.js
+++ renderer.js
@@ -3,19 +3,27 @@ const tmdb_top = "movie/top_rated";
 const tmdb_trendind = "trending/movie/week";
 const tmdb_image = "https://image.tmdb.org/t/p/w500";
 const locale = "pt-BR";
-const api_key = "0f6d8485e4e96212071d0548796a9818";
 
 const omdb_url = "http://www.omdbapi.com/";
+const api_key = "0f6d8485e4e96212071d0548796a9818";
 const api_keyOMDb = "a0eda058";
 
 const el_movies = document.querySelector(".movies");
 
+/**
+ *
+ * Realiza o fetch a API e retorna o JSON da resposta
+ */
 async function requestAPI(request) {
   const response = await fetch(request);
   const data = await response.json();
   return data;
 }
 
+/**
+ *
+ *
+ */
 async function getMovies() {
   // let request = new Request(
   //   tmdb_url + tmdb_trendind + "?api_key=" + api_key + "&language=" + locale
@@ -25,6 +33,10 @@ async function getMovies() {
   return data["results"];
 }
 
+/**
+ *
+ * Gera uma Array de ratings a partir do request a API do OMdb
+ */
 async function getRatingsOMdbApi(movies) {
   let ratingsOMdb = [];
 
@@ -52,6 +64,10 @@ async function getRatingsOMdbApi(movies) {
   return ratingsOMdb;
 }
 
+/**
+ *
+ * Calcula o score de todos os filmes com base nas avaliações do TMDb e as do OMDB
+ */
 function scoreEval(movies, ratings) {
   let score = [];
   for (let i = 0; i < movies.length; i++) {
@@ -70,6 +86,10 @@ function scoreEval(movies, ratings) {
   return score;
 }
 
+/**
+ *
+ * Padrozina a Rating de um filme
+ */
 function convertRating(string) {
   let rating;
 
@@ -81,27 +101,54 @@ function convertRating(string) {
   return rating;
 }
 
+/**
+ *
+ * Injeta o trecho com os dados dos filmes no HTML
+ */
 function outputMovies(movies) {
   let output = "";
-  movies.forEach((movie, index) => {
+  movies.forEach((element, index) => {
+    const tmdb = element["movie"]["vote_average"];
+    const ratings = element["ratings"];
+    let imdb = "-";
+    let rotten = "-";
+    let meta = "-";
+    if (ratings)
+      ratings.forEach((r) => {
+        if (r[0] == "imdb") imdb = r[1];
+        if (r[0] == "rotten") rotten = r[1];
+        if (r[0] == "meta") meta = r[1];
+      });
+
+    console.log(ratings);
     output += `
               <div>
-                <img src=${tmdb_image}${movie["poster_path"]} alt="Poster ${movie["title"]}"/>
-                <h2>${movie["title"]}</h2>
-                <p>${movie["vote_average"]}</p>
+                <img src=${tmdb_image}${element["movie"]["poster_path"]} alt="Poster ${element["movie"]["title"]}"/>
+                <h2>${element["movie"]["title"]}</h2>
+                <div class="ratings">
+                  <p>${element["score"]}</p>
+                  <p>${tmdb}</p>
+                  <p>${imdb}</p>
+                  <p>${rotten}</p>
+                  <p>${meta}</p>
+                </div>
               </div>
               `;
   });
   el_movies.innerHTML = output;
 }
 
-function sortMovies(movies,ratings,score) {
+/**
+ *
+ * Ordena os filmes em ordem decrescente de acordo com o score
+ */
+function sortMovies(movies, ratings, score) {
   let unsorted = new Array();
   for (let index = 0; index < score.length; index++) {
     unsorted.push({
       score: score[index],
       movie: movies[index],
-      ratings: ratings,
+      ratings: ratings[index],
     });
   }
 
@@ -118,12 +165,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     const omdb_ratings = await getRatingsOMdbApi(movies);
     let score = scoreEval(movies, omdb_ratings);
 
-    console.log(score);
-    const sorted = sortMovies(movies,omdb_ratings,score);
-    console.log(sorted);
+    const sorted = sortMovies(movies, omdb_ratings, score);
 
-    outputMovies(movies);
-
+    outputMovies(sorted);
   } catch (er) {
     console.log("Erro");
     console.log(er);
